c618cffd1557118b00a1da28d4be3dd47be72992
Tomas Grubliauskas <v3nom@MacPro.local>
2019-03-31 13:20:26 +0200

    initial code commit

---
+++ gorest.go
@@ -0,0 +1,49 @@
+package gorest
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	"google.golang.org/appengine/log"
+)
+
+// Error struct for API error
+type Error struct {
+	Message string `json:"message"`
+}
+
+// Response struct for API response
+type Response struct {
+	Status int         `json:"status"`
+	Error  *Error      `json:"error"`
+	Data   interface{} `json:"data"`
+}
+
+// ReturnAPIError creates API error response
+func ReturnAPIError(ctx context.Context, w http.ResponseWriter, err error) error {
+	var response Response
+	response.Status = 1
+	response.Error = &Error{
+		Message: err.Error(),
+	}
+	jsonErr := json.NewEncoder(w).Encode(response)
+	if jsonErr != nil {
+		log.Errorf(ctx, "Error json err: %v, error: %v", jsonErr, err)
+	}
+	return err
+}
+
+// ReturnAPIResponse creates API response
+func ReturnAPIResponse(ctx context.Context, w http.ResponseWriter, data interface{}) error {
+	var response Response
+	response.Status = 0
+	response.Error = nil
+	response.Data = data
+	jsonErr := json.NewEncoder(w).Encode(response)
+	if jsonErr != nil {
+		log.Errorf(ctx, "Response json err: %v, data: %#v", jsonErr, data)
+	}
+	return jsonErr
+}
